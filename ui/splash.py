6b1d050b9c2df45186dc0997038d8b61d8c8acdb
Jon <jonnythejet@gmail.com>
2026-08-05 14:13:50 -0700

    - Updating Copyright notices

--- ui/splash.py
+++ ui/splash.py
@@ -1,20 +1,18 @@
-#  Copyright (c) 2024 Jon Evans.
+#      TinyType is a minimal typing test software that sits in the corner of your screen while you work!
+#      Copyright (C) 2026  Jon Evans
 #
-#  The original Wwise-Python Tool Template and source code is provided by Jon Evans,
-#  Copyright 2024 (c) Jon Evans Audio under the Apache License, Version 2.0
-#  for the purposes of distributing internal tools
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from pathlib import Path
 
